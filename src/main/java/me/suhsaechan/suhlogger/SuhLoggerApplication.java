923480d266b6fed956a16dde546e5ca52035ba4a
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-04-19 22:26:24 +0900

    메소드 로깅 커스텀 어노테이션 추가 : feat : SuperLog에 ClassType 출력하지않는 옵션값 추가 테스트코드추가, 기본 어노테이션 출력값 추가 https://github.com/Cassiiopeia/suh-logger/issues/4

--- src/main/java/me/suhsaechan/suhlogger/SuhLoggerApplication.java
+++ src/main/java/me/suhsaechan/suhlogger/SuhLoggerApplication.java
@@ -2,10 +2,10 @@ package me.suhsaechan.suhlogger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class SuhLoggerApplication {
 
   public static void main(String[] args) {
