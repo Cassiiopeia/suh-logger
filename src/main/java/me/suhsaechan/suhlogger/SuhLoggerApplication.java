0868489b84f9dff15f6034ede0c523a809baf476
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-04-19 19:45:27 +0900

    프로젝트 기본 골격 추가 : feat : 프로젝트 기본 파일 추가 https://github.com/Cassiiopeia/suh-logger/issues/1

---
+++ src/main/java/me/suhsaechan/suhlogger/SuhLoggerApplication.java
@@ -0,0 +1,13 @@
+package me.suhsaechan.suhlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhLoggerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SuhLoggerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/suhsaechan/suhlogger/SuhLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.suhsaechan.suhlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhLoggerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
